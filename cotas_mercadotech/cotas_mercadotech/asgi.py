5584cb7c1f59d6eca5428211c051ed794b110303
Vitor Vicente <vitor.email2011@gmail.com>
2020-09-24 19:06:41 -0300

    start do projeto no django

---
+++ cotas_mercadotech/cotas_mercadotech/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cotas_mercadotech project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cotas_mercadotech.settings')
+
+application = get_asgi_application()
